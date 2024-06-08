d7cee37ee374a51bf383d6fa21eae4982e563f32
its-filip-melka <133359834+its-filip-melka@users.noreply.github.com>
2024-06-08 16:59:08 +0200

    feat: process response and create questions array (using dummy response)

--- scripts/openai.js
+++ scripts/openai.js
@@ -1,37 +1,38 @@
 import { OPENAI_KEY } from '../openaiConfig.js'
 
-const data = [
-	{
-		question: 'What is Hashnode primarily known for?',
-		options: [
-			'A) Social networking',
-			'B) Blogging platform',
-			'C) E-commerce',
-			'D) Cloud storage',
-		],
-		correctAnswer: 'B',
-	},
-	{
-		question: 'Which of the following features is NOT offered by Hashnode?',
-		options: [
-			'A) Custom domain support',
-			'B) Built-in newsletter integration',
-			'C) Code collaboration tools',
-			'D) SEO optimization features',
-		],
-		correctAnswer: 'C',
-	},
-	{
-		question: 'Which of these is a key benefit of using Hashnode for blogging?',
-		options: [
-			'A) Access to an exclusive audience',
-			'B) Requires no coding knowledge to set up',
-			'C) Offers cloud storage solutions',
-			'D) Provides e-commerce integration',
-		],
-		correctAnswer: 'B',
-	},
-]
+const exampleResponse = `
+Question: Where is the code related to the header section shown in the image located at?
+A) layout.tsx
+B) site-header.tsx
+C) footer.tsx
+D) index.tsx
+Correct answer: A
+
+Question: Which component is responsible for the search functionality in the header?
+A) MainNav
+B) MobileNav
+C) CommandMenu
+D) ModeToggle
+Correct answer: C
+
+Question: Which file contains the code snippet for the header section?
+A) layout.tsx
+B) index.tsx
+C) site-header.tsx
+D) footer.tsx
+Correct answer: C
+
+Question: Who is the author of the article providing information on the header development?
+A) Ramu Narasinga
+B) John Doe
+C) Jane Smith
+D) Michael Johnson
+Correct answer: A
+`
+
+const questionRegex = /^Question.*$/gm
+const optionsRegex = /^([ABCD]\) ).*$/gm
+const correctAnswerRegex = /^Correct answer: [ABCD]$/gm
 
 const prompt = `
 Could you come up with multiple choice questions based on the markdown content provided? Here is an example format:
@@ -45,8 +46,7 @@ Correct answer: B
 `
 
 export async function fetchQuestions(markdown) {
-	console.log(markdown)
-	const message = markdown + '\n' + prompt
+	/* const message = markdown + '\n' + prompt
 
 	const response = await fetch('https://api.openai.com/v1/chat/completions', {
 		method: 'POST',
@@ -64,10 +64,27 @@ export async function fetchQuestions(markdown) {
 			],
 			max_tokens: 200,
 		}),
-	}).then((res) => res.json())
+	}).then((res) => res.json()).then(data => data.choices[0].message.content)
 
-	console.log(response)
-	console.log(response.choices[0].message.content)
+	console.log(response) */
+
+	const data = []
+
+	exampleResponse.split('\n\n').forEach((section) => {
+		const question = section.match(questionRegex)[0]
+		const options = section.match(optionsRegex)
+		const correctAnswer = section.match(correctAnswerRegex)[0]
+
+		if (question.length > 0 && options.length > 0 && correctAnswer.length > 0) {
+			data.push({
+				question: question.substring(question.indexOf(' ') + 1),
+				options,
+				correctAnswer: correctAnswer.slice(-1),
+			})
+		}
+	})
+
+	console.log(data)
 
 	return data
 }
