9b5cb71d0462693470b23e9d0aea5630e67813c7
its-filip-melka <133359834+its-filip-melka@users.noreply.github.com>
2024-06-09 09:16:16 +0200

    feat: create a POST request to the server to handle openai requests

--- extension/scripts/openai.js
+++ extension/scripts/openai.js
@@ -1,63 +1,15 @@
-import { OPENAI_KEY } from '../openaiConfig.js'
-
-const questionRegex = /^Question.*$/gm
-const optionsRegex = /^([ABCD]\) ).*$/gm
-const correctAnswerRegex = /^Correct answer: [ABCD]$/gm
-
-const prompt = `
-Could you come up with multiple choice questions based on the markdown content provided? Here is an example format:
-
-Question: What is Hashnode primarily known for?
-A) Social networking
-B) Blogging platform
-C) E-commerce
-D) Cloud storage
-Correct answer: B
-`
-
 export async function fetchQuestions(markdown) {
-	const message = markdown + '\n' + prompt
-
-	const response = await fetch('https://api.openai.com/v1/chat/completions', {
+	console.log(markdown)
+	const { questions } = await fetch('http://localhost:4000/openai/questions', {
 		method: 'POST',
-		headers: {
-			'Content-Type': 'application/json',
-			Authorization: `Bearer ${OPENAI_KEY}`,
-		},
 		body: JSON.stringify({
-			model: 'gpt-3.5-turbo',
-			messages: [
-				{
-					role: 'user',
-					content: message,
-				},
-			],
-			max_tokens: 200,
+			markdown,
 		}),
-	})
-		.then((res) => res.json())
-		.then((data) =>
-			data.choices[0].message.content.replaceAll(
-				/\[([^\]]+)\]\([^\)]+\)/g,
-				'$1'
-			)
-		)
+		headers: {
+			Accept: 'application/json',
+			'Content-Type': 'application/json',
+		},
+	}).then((res) => res.json())
 
-	const data = []
-
-	response.split('\n\n').forEach((section) => {
-		const question = section.match(questionRegex)
-		const options = section.match(optionsRegex) || []
-		const correctAnswer = section.match(correctAnswerRegex)
-
-		if (question && options.length > 0 && correctAnswer) {
-			data.push({
-				question: question[0].substring(question[0].indexOf(' ') + 1),
-				options,
-				correctAnswer: correctAnswer[0].slice(-1),
-			})
-		}
-	})
-
-	return data
+	return questions
 }
